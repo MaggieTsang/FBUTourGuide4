4ea0d1bfa87dcaf3e3bd9517a3b33cae3235a8e5
Erica Marie Seto Kok <ekok@fb.com>
2016-07-07 20:55:22 -0700

    more updates

--- app/src/main/java/com/example/ekok/fbutourguideapp/Login/UserLogin.java
+++ app/src/main/java/com/example/ekok/fbutourguideapp/Login/UserLogin.java
@@ -3,7 +3,6 @@ package com.example.ekok.fbutourguideapp.Login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Toast;
 
 import com.example.ekok.fbutourguideapp.R;
@@ -11,6 +10,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -35,26 +35,34 @@ public class UserLogin extends AppCompatActivity {
 
         callbackManager = CallbackManager.Factory.create();
 
+        if (Profile.getCurrentProfile().getId() != null) {
+            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    // App code
+                    onLoggedIn();
+                }
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                // App code
-                Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
-            }
+                @Override
+                public void onCancel() {
+                    // App code
+                    Toast.makeText(getApplicationContext(), "Login cancelled", Toast.LENGTH_SHORT).show();
+                }
 
-            @Override
-            public void onCancel() {
-                // App code
-            }
+                @Override
+                public void onError(FacebookException exception) {
+                    // App code
+                    Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_SHORT).show();
+                    exception.printStackTrace();
+                }
+            });}
 
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-                Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_SHORT).show();
-                exception.printStackTrace();
-            }
-        });
+        else
+        {
+            onLoggedIn();
+        }
+
+
     }
 
     @Override
@@ -62,7 +70,9 @@ public class UserLogin extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void launchUserType(View view) {
+    public void onLoggedIn() {
+        String name = Profile.getCurrentProfile().getFirstName();
+        Toast.makeText(getApplicationContext(), "Welcome, " + name + "!", Toast.LENGTH_SHORT).show();
         Intent i = new Intent(this, UserType.class);
         startActivity(i);
     }
