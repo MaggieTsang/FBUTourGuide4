12bef1092cd6df795b28ce900b70f6c5ff5de741
Maggie Tsang <mbytsang@fb.com>
2016-07-12 11:49:06 -0700

    pull new login

--- app/src/main/java/com/example/ekok/fbutourguideapp/Guides/GuideBasic.java
+++ app/src/main/java/com/example/ekok/fbutourguideapp/Guides/GuideBasic.java
@@ -1,6 +1,7 @@
 package com.example.ekok.fbutourguideapp.Guides;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -12,6 +13,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.ekok.fbutourguideapp.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.FileNotFoundException;
 
@@ -23,6 +26,8 @@ public class GuideBasic extends AppCompatActivity{
     GuideUser guideUser;
 
     ImageView ivProfilePic;
+    DatabaseReference dataBaseRef;
+    GuideFirebase guideFirebase;
 
     EditText etName;
     EditText etLocation;
@@ -35,17 +40,26 @@ public class GuideBasic extends AppCompatActivity{
         //if new --> guidenew
         //if registered --> guiderequests
         setContentView(R.layout.activity_guidebasic);
+
+
+        dataBaseRef = FirebaseDatabase.getInstance().getReference();
+        guideFirebase = new GuideFirebase(dataBaseRef);
+
+
+
         guideUser = (GuideUser) getIntent().getSerializableExtra("guideUser");
 
         etName = (EditText) findViewById(R.id.etName);
         etLocation = (EditText) findViewById(R.id.etLocation);
         etBasicAdditional = (EditText) findViewById(R.id.etBasicAdditional);
         etLanguages = (EditText) findViewById(R.id.etLanguages);
+
         etName.setText(guideUser.legalName);
         etLocation.setText(guideUser.location);
         etBasicAdditional.setText(guideUser.description);
         etLanguages.setText(guideUser.languages);
 
+        //Go to camera roll to upload profile pic
         ivProfilePic = (ImageView) findViewById(R.id.ivProfilePic);
         Button loadImage = (Button) findViewById(R.id.btnUploadImage);
         loadImage.setOnClickListener(new Button.OnClickListener(){
@@ -59,6 +73,32 @@ public class GuideBasic extends AppCompatActivity{
 
     }
 
+    //Launches Contact info view and saves current info to FireBase
+    public void launchContact(View v) {
+
+        guideUser.legalName = etName.getText().toString();
+        guideUser.location = etLocation.getText().toString();
+        guideUser.description = etBasicAdditional.getText().toString();
+        guideUser.languages = etLanguages.getText().toString();
+        guideFirebase.saveToGuide(guideUser);
+
+
+        String guideName = findViewById(R.id.etName).toString();
+        SharedPreferences prefs = getSharedPreferences("my_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putString("etName", guideName );
+        edit.commit();
+
+
+        Intent i = new Intent(this, GuideContact.class);
+        i.putExtra("guideUser", guideUser);
+        startActivity(i);
+    }
+
+
+
+
+    //Load a profile picture
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -74,21 +114,6 @@ public class GuideBasic extends AppCompatActivity{
                 e.printStackTrace();
             }
         }
-    }
-
-
-
-
-    public void launchContact(View v) {
-        guideUser.legalName = etName.getText().toString();
-        guideUser.location = etLocation.getText().toString();
-        guideUser.description = etBasicAdditional.getText().toString();
-        guideUser.languages = etLanguages.getText().toString();
-
-        // first parameter is the context, second is the class of the activity to launch
-        Intent i = new Intent(this, GuideContact.class);
-        i.putExtra("guideUser", guideUser);
-        startActivity(i); // brings up the second activity
     }
 
 }
