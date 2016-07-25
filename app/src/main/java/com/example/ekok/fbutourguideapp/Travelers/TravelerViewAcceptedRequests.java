5499468616c3c6cc00217f6ce29f43966954abfb
Erica Marie Seto Kok <ekok@fb.com>
2016-07-25 10:31:31 -0700

    more more stuff works

---
+++ app/src/main/java/com/example/ekok/fbutourguideapp/Travelers/TravelerViewAcceptedRequests.java
@@ -0,0 +1,15 @@
+package com.example.ekok.fbutourguideapp.Travelers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ekok.fbutourguideapp.R;
+
+public class TravelerViewAcceptedRequests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travelerviewacceptedrequests);
+    }
+}
--- app/src/main/java/com/example/ekok/fbutourguideapp/Travelers/TravelerViewTrips.java
+++ app/src/main/java/com/example/ekok/fbutourguideapp/Travelers/TravelerViewTrips.java
@@ -100,32 +100,6 @@ public class TravelerViewTrips extends AppCompatActivity{
                     lvTrips.setAdapter(tripsAdapter);
                 }
 
-//                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                    @Override
-//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//            /*//listview clicked item index
-//            int itemPosition = position;
-//
-//            //Listview clicked item value
-//            String itemValue = (String)listView.getItemAtPosition(position);
-//
-//            //show alert
-//            Toast.makeText(getApplicationContext(),
-//                    "Position:"+itemPosition+" ListItem:" + itemValue, Toast.LENGTH_LONG).show();*/
-//                        switch(itemPosition)
-//
-//                        case 0 :Intent appInfo = new Intent(About.this, about_app.class);
-//                            startActivity(appInfo);
-//                            break;
-//                        case 1 :Intent appInfo = new Intent(About.this, Activity1.class);
-//                            startActivity(appInfo);
-//                            break;
-//                        case 2 :Intent appInfo = new Intent(About.this, Activity2.class);
-//                            startActivity(appInfo);
-//                            break;
-//                    }
-//                });
-
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
                     Toast.makeText(TravelerViewTrips.this, "Error.", Toast.LENGTH_SHORT).show();
@@ -157,5 +131,10 @@ public class TravelerViewTrips extends AppCompatActivity{
             RequestModel requestModel = new RequestModel();
             String place = data.getExtras().getString("place");
         }
+    }
+
+    public void viewAcceptedReqs(MenuItem item) {
+        Intent i = new Intent(this, TravelerViewAcceptedRequests.class);
+        startActivity(i);
     }
 }
